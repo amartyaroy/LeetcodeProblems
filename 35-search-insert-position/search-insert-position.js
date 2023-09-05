538f92ccf587ac1e1cabdc1df525c34ec22bd463
Amartya Kumar Roy <amartyaroy.work@gmail.com>
2023-09-05 22:18:33 +0530

    Time: 45 ms (91.00%) | Memory: 42.3 MB (37.62%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -5,12 +5,14 @@
  */
 var searchInsert = function(nums, target) {
     for(i=0;i<nums.length;i++){
-            if(target==nums[i]){
-                return i;
-            }
-            else if(target<nums[i]){
-                return i;
-            }
+        if((nums[i] == target) || (target < nums[i])){
+            return i
+        }
+        if(target < nums[i+1]){
+            return (i+1)
+        }
+        if(target > nums[nums.length - 1]){
+            return (nums.length)
+        }
     }
-        return nums.length;
 };
